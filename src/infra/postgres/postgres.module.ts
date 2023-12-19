f465854a4bdd71febf679aed4094ae7ac681069d
relby <kudinov.nikita@gmail.com>
2023-12-19 21:08:07 +0300

    feat: add `forFeature` method for `PostgresModule`

--- src/infra/postgres/postgres.module.ts
+++ src/infra/postgres/postgres.module.ts
@@ -3,13 +3,22 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvVariables } from '../config/validation';
 import { TypeOrmConfigService } from '../config/typeorm';
+import { ConfigModule } from "@nestjs/config";
+import { validate } from '../config/validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validate,
+    }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService<EnvVariables>],
       useClass: TypeOrmConfigService,
     }),
   ],
 })
-export class PostgresModule {}
+export class PostgresModule {
+  public static forFeature = TypeOrmModule.forFeature;
+}
